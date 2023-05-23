682505d2b6e8b9b48a824c830e63e12edfa9d032
Westermeister <westermeister12@gmail.com>
2023-05-22 21:09:37 -0700

    Use identity for primary key generation strategy

--- src/main/java/com/westermeister/news/entity/User.java
+++ src/main/java/com/westermeister/news/entity/User.java
@@ -3,6 +3,7 @@ package com.westermeister.news.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -13,7 +14,7 @@ import jakarta.persistence.Table;
 @Table(name = "user_")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", length = 255)
