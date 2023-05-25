36afff44f9da58103abd67e3754ca59acdb2a7aa
Westermeister <westermeister12@gmail.com>
2023-05-25 00:32:33 -0700

    Add deletion cascade from user to session

--- src/main/java/com/westermeister/news/entity/User.java
+++ src/main/java/com/westermeister/news/entity/User.java
@@ -1,10 +1,16 @@
 package com.westermeister.news.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 /**
@@ -31,6 +37,9 @@ public class User {
 
     @Column(name = "last_sign_in")
     private Long lastSignIn;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Session> sessions = new ArrayList<>();
 
     protected User() {}
 
