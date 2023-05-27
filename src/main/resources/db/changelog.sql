462cc57c8ce9c8f64e5e9c5d1f664d04a467d25a
Westermeister <westermeister12@gmail.com>
2023-05-26 20:02:08 -0700

    Migrate changelog from json to sql

---
+++ src/main/resources/db/changelog.sql
@@ -0,0 +1,21 @@
+--liquibase formatted sql
+
+--changeset westermeister:1
+CREATE TABLE user_ (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    created BIGINT,
+    last_sign_in BIGINT
+);
+
+--changeset westermeister:2
+CREATE TABLE session (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT,
+    token VARCHAR(255),
+    created BIGINT,
+    expires BIGINT,
+    CONSTRAINT fk_session_user_id_user_ FOREIGN KEY (user_id) REFERENCES user_ (id)
+);
