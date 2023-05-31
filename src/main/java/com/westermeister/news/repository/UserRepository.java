a84022e8118ff780ad95a4372193f65547f18e95
Westermeister <westermeister12@gmail.com>
2023-05-30 18:30:30 -0700

    Add email query method

--- src/main/java/com/westermeister/news/repository/UserRepository.java
+++ src/main/java/com/westermeister/news/repository/UserRepository.java
@@ -3,9 +3,17 @@ package com.westermeister.news.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.westermeister.news.entity.User;
+import java.util.List;
 
 /**
  * CRUD interface for interacting with the user table.
  */
 public interface UserRepository extends CrudRepository<User, Long> {
+    /**
+     * Find user by email.
+     *
+     * @param email  user's email
+     * @return       list of users
+     */
+    List<User> findByEmail(String email);
 }
