44f4f243a69288430a3ac61eb55c373d7f5d0a78
Westermeister <westermeister12@gmail.com>
2023-06-01 18:51:17 -0700

    Add sass plugin to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import io.freefair.gradle.plugins.sass.SassExtension
+import sass.embedded_protocol.EmbeddedSass.OutputStyle
+
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.0"
-	id("io.spring.dependency-management") version "1.1.0"
+    java
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("io.freefair.sass-base") version "8.0.1"
+    id("io.freefair.sass-webjars") version "8.0.1"
+    id("io.freefair.sass-java") version "8.0.1"
 }
 
 group = "com.westermeister"
@@ -9,26 +15,35 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.bouncycastle:bcprov-jdk18on:1.73")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.liquibase:liquibase-core")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.webjars:bootstrap:5.1.3")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.73")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.liquibase:liquibase-core")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.getByName<Jar>("jar") {
-	enabled = false
+    enabled = false
+}
+
+configure<SassExtension> {
+    omitSourceMapUrl.set(false)
+    outputStyle.set(OutputStyle.COMPRESSED)
+    sourceMapContents.set(false)
+    sourceMapEmbed.set(false)
+    sourceMapEnabled.set(true)
 }
