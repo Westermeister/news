2a2e5218c2ac537bf8a1bafa57480d928d75f7b1
Westermeister <westermeister12@gmail.com>
2023-06-03 18:50:28 -0700

    Add form object for signing up

---
+++ src/main/java/com/westermeister/news/form/SignUpForm.java
@@ -0,0 +1,68 @@
+package com.westermeister.news.form;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Form for creating new users.
+ *
+ * @see com.westermeister.news.entity.User corresponding entity class
+ */
+public class SignUpForm {
+    @NotNull
+    @Size(min=1, message="Enter your full name.")
+    @Size(max=255, message="Shorten your name to 255 characters or less, or just use your first name.")
+    private String name;
+
+    @NotNull
+    @Size(min=1, message="Enter your email address.")
+    @Size(max=255, message="Provide an email that is 255 characters or less.")
+    private String email;
+
+    @NotNull
+    @Size(min=8, message="Enter at least 8 characters.")
+    @Size(max=255, message="Shorten your password to 255 characters or less.")
+    private String password;
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @see com.westermeister.news.entity.User corresponding entity class
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
