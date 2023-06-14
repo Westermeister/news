671248285a682dd972497c2dd5e186b773bdb58c
Westermeister <westermeister12@gmail.com>
2023-06-13 22:32:48 -0700

    Implement UserDetailsService in security config

--- src/main/java/com/westermeister/news/security/SecurityConfiguration.java
+++ src/main/java/com/westermeister/news/security/SecurityConfiguration.java
@@ -1,14 +1,28 @@
 package com.westermeister.news.security;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import com.westermeister.news.entity.User;
+import com.westermeister.news.repository.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Spring Security configuration class.
@@ -16,6 +30,19 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private UserRepository userRepo;
+    private HttpServletRequest request;
+
+    /**
+     * Inject dependencies.
+     *
+     * @param userRepo  used to load user details
+     */
+    public SecurityConfiguration(UserRepository userRepo, HttpServletRequest request) {
+        this.userRepo = userRepo;
+        this.request = request;
+    }
+
     /**
      * Define list of security filters before allowing a request through.
      *
@@ -50,10 +77,42 @@ public class SecurityConfiguration {
                 .logoutUrl("/signout")
                 .permitAll()
             )
-            .sessionManagement(session -> session
+            .sessionManagement((session) -> session
                 .maximumSessions(10)
             );
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return (username) -> {
+            if (request.getRequestURI().equals("/signin")) {
+                // Throttle authentication to mitigate brute force attacks.
+                // On average, signing in will take 4 to 5 seconds.
+                int randomDelay = ThreadLocalRandom.current().nextInt(3, 6);
+                try {
+                    TimeUnit.SECONDS.sleep(randomDelay);
+                } catch (InterruptedException e) {
+                    System.err.format("Couldn't sleep because of error: %s%n", e.toString());
+                }
+            }
+
+            String email = username;
+            List<User> users = userRepo.findFirstByEmail(email);
+            if (users.isEmpty()) {
+                String message = String.format("User not found: %s", email);
+                throw new UsernameNotFoundException(message);
+            }
+
+            User user = users.get(0);
+            String password = user.getPassword();
+            List<GrantedAuthority> roles = new ArrayList<>();
+            roles.add(new SimpleGrantedAuthority(user.getRole()));
+
+            // Below, we use the user ID, instead of the email, for Spring Security's notion of a "username".
+            // This is because we want to allow the user to change their email without requiring re-authentication.
+            return new org.springframework.security.core.userdetails.User(user.getId().toString(), password, roles);
+        };
     }
 
     /**
