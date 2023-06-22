79777307fecff907d32393263afcf34f01397a36
Westermeister <westermeister12@gmail.com>
2023-06-21 20:07:02 -0700

    Enable scheduling

--- src/main/java/com/westermeister/news/NewsApplication.java
+++ src/main/java/com/westermeister/news/NewsApplication.java
@@ -2,11 +2,13 @@ package com.westermeister.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Application entry point.
  */
 @SpringBootApplication
+@EnableScheduling
 public class NewsApplication {
     /**
      * Start the Spring app.
